665ad2d8ecd7f0d1bdbc09d07617b9876e3626c0
Gurucharan M <maripalagurucharan@gmail.com>
2025-08-27 15:12:38 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,32 +3,27 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: false,
-        },
-        "/openai-api": {
-          target: "http://localhost:3001",
-          changeOrigin: true,
-          secure: false,
-        },
-        "/fastapi": {
-          target: "http://127.0.0.1:8000",
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/fastapi/, ""),
-        },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
       },
+      "/openai-api":{
+        target:"http://localhost:3001",
+        changeOrigin:true,
+        secure:false
+      },
+      "/fastapi":{
+        target:"http://127.0.0.1:8000",
+        changeOrigin:true,
+        secure:false,
+        rewrite: (path) => path.replace(/^\/fastapi/, ""),
+      }
     },
-    // 👇 dynamic base
-    base: isGithubPages ? "/blog-chey/" : "/",
-  };
+  },
+  base: process.env.VITE_BASE_PATH || "/blog-chey/"
 });
